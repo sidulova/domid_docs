8d94946449cdc9b0188da8de09635a37f4fe3d99
sidulova <100537985+sidulova@users.noreply.github.com>
2022-06-14 18:33:59 -0400

    Add files via upload

--- domid/algos/builder_vade.py
+++ domid/algos/builder_vade.py
@@ -1,12 +1,13 @@
 from libdg.algos.a_algo_builder import NodeAlgoBuilder
-from libdg.algos.trainers.train_basic import TrainerBasic
+#from libdg.algos.trainers.train_basic import TrainerBasic
+from domid.trainers.trainer_vade import TrainerVADE
 from libdg.algos.msels.c_msel import MSelTrLoss
 from libdg.algos.msels.c_msel_oracle import MSelOracleVisitor
 from libdg.algos.observers.c_obvisitor_cleanup import ObVisitorCleanUp
 from libdg.utils.utils_cuda import get_device
 
 from domid.algos.observers.b_obvisitor_clustering_only import ObVisitorClusteringOnly
-from domid.models.model_vade import ModelVaDE
+from domid.models.model_vade_v2 import ModelVaDE
 
 class NodeAlgoBuilderVaDE(NodeAlgoBuilder):
     def init_business(self, exp):
@@ -17,12 +18,19 @@ class NodeAlgoBuilderVaDE(NodeAlgoBuilder):
         args = exp.args
         device = get_device(args.nocu)
         # FIXME: add the nevessary function arguments:
+        y_dim=len(task.list_str_y)
+        print(y_dim)
+        zd_dim = args.zd_dim
+        y_dim = len(task.list_str_y),
+
+
 
         #model = ModelVaDE(device=device, args=args)
-        model = ModelVaDE(y_dim=y_dim, zd_dim=zd_dim, device=device)
+        model = ModelVaDE(y_dim=y_dim, zd_dim=zd_dim, device=device,  i_c = task.isize.c,
+                          i_h = task.isize.h, i_w = task.isize.w, gamma_y = args.gamma_y,list_str_y = task.list_str_y, dim_feat_x = 10)
         observer = ObVisitorCleanUp(
             ObVisitorClusteringOnly(exp, MSelOracleVisitor(MSelTrLoss(max_es=args.es)), device))
-        trainer = TrainerBasic(model, task, observer, device, aconf=args)
+        trainer = TrainerVADE(model, task, observer, device, aconf=args)
         return trainer
 
 

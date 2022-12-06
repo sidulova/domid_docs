806471ba45e479401ccd632db837376dce335d63
agisga <11449372+agisga@users.noreply.github.com>
2022-12-06 14:23:00 -0500

    another bug fix

--- domid/trainers/pretraining_vade.py
+++ domid/trainers/pretraining_vade.py
@@ -42,10 +42,15 @@ class Pretraining():
         Z = np.zeros((num_img, self.model.zd_dim))
         counter = 0
         with torch.no_grad():
-            for tensor_x, vec_y, vec_d,*other_vars  in self.loader_tr:
-                tensor_x = tensor_x.to(self.device)
+            for tensor_x, vec_y, vec_d, *other_vars in self.loader_tr:
                 if len(other_vars)>0:
-                    machine, img_locs, pred_domain = other_vars #Because of the MNIST color is implemented rn
+                    machine, img_locs, pred_domain = other_vars  #Because of the MNIST color is implemented rn
+
+                tensor_x, vec_y, vec_d = (
+                    tensor_x.to(self.device),
+                    vec_y.to(self.device),
+                    vec_d.to(self.device),
+                )
 
                 if not self.is_inject_domain:
                     inject_tensor = []
@@ -54,6 +59,7 @@ class Pretraining():
                     # both class label and domain label or only class label, or
                     # nothing
                     if len(pred_domain) > 1:
+                        pred_domain = pred_domain.to(self.device)
                         if vec_y.shape[1] + pred_domain.shape[1] == self.args.dim_inject_y:
                             inject_tensor = torch.cat(vec_y, pred_domain)
                         else:
